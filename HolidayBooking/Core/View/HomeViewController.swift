7c35ef4fbe9057710eb25e8c3400a90fdb584c6c
guncel-aldemir <guncelaldemir@gmail.com>
2023-12-12 16:23:38 +0300

    Initial Commit

---
+++ HolidayBooking/Core/View/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HolidayBooking
+//
+//  Created by guncel on 12.12.2023.
+//
+
+import UIKit
+
+
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ HolidayBooking/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  HolidayBooking
+//
+//  Created by guncel on 12.12.2023.
+//
+
+import Foundation
